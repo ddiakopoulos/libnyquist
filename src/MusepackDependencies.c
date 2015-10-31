caddc84b19aeea8ab4780426c3a0d79cd685bb14
Dimitri Diakopoulos <ddiakopoulos@gmail.com>
2015-10-31 10:54:52 -0700

    compiling musepack dependencies into libnyquist - xcode specific at the moment

---
+++ src/MusepackDependencies.c
@@ -0,0 +1,56 @@
+/*
+Copyright (c) 2015, Dimitri Diakopoulos All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#if (_MSC_VER)
+    #pragma warning (push)
+    #pragma warning (disable: 181 111 4267 4996 4244 4701 4702 4133 4100 4127 4206 4312 4505 4365 4005 4013 4334)
+#endif
+
+#ifdef __clang__
+    #pragma clang diagnostic push
+    #pragma clang diagnostic ignored "-Wconversion"
+    #pragma clang diagnostic ignored "-Wshadow"
+    #pragma clang diagnostic ignored "-Wdeprecated-register"
+#endif
+
+#include "musepack/libmpcdec/huffman.c"
+
+//#include "musepack/libmpcdec/mpc_decoder.c"
+//#include "musepack/libmpcdec/mpc_reader.c"
+//#include "musepack/libmpcdec/mpc_bits_reader.c"
+//#include "musepack/libmpcdec/mpc_demux.c"
+
+#include "musepack/libmpcdec/requant.c"
+#include "musepack/libmpcdec/streaminfo.c"
+#include "musepack/libmpcdec/synth_filter.c"
+#include "musepack/libmpcdec/crc32.c"
+
+#ifdef __clang__
+    #pragma clang diagnostic pop
+#endif
+
+#if (_MSC_VER)
+    #pragma warning (pop)
+#endif
