29b7b203f9d4de1a8c167b8a43122012a6cc04bc
Dimitri <ddiakopoulos@gmail.com>
2016-02-11 20:42:41 -0800

    introduce vs 2015 example solution/proj file and fix paths. vs 2013 needs to be fixed

--- src/OpusDependencies.c
+++ src/OpusDependencies.c
@@ -92,16 +92,9 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "opus/celt/rate.c"
 #include "opus/celt/vq.c"
 
-// Disabled inline because of name clash of opus_custom_encoder_get_size.
 #include "opus/celt/celt_decoder.c"
 #define opus_custom_encoder_get_size opus_custom_encoder_get_size_alt
 #include "opus/celt/celt_encoder.c"
-
-/*
-    See celt/celt_decoder.c + celt/celt_encoder.c in the project browser.
-    These files need to be in separate translation units due to name clashes,
-    unfortunately.
-*/
 
 /////////////////
 // SILK Common //
